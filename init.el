7453372febfa3c7249146c27656bcc041185e12b
Akshay Mishra <lck.akshay@gmail.com>
2015-08-24 14:24:51 -0500

    Emacs config based on org mode.

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
 (require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(background-color "#202020")
+ '(background-mode dark)
+ '(cursor-color "#cccccc")
+ '(foreground-color "#cccccc")
+ '(linum-format "%3i")
+ '(magit-popup-use-prefix-argument (quote default))
+ '(powerline-color1 "#3d3d68")
+ '(powerline-color2 "#292945"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

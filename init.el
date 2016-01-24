a5c405d9693c9336d1013c871e9878ee6acbcfbd
Akshay Mishra <lck.akshay@gmail.com>
2016-01-23 23:29:28 -0600

    helm tweaks

--- init.el
+++ init.el
@@ -10,8 +10,12 @@
  '(background-mode dark)
  '(cursor-color "#cccccc")
  '(foreground-color "#cccccc")
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
  '(linum-format "%3i")
  '(magit-popup-use-prefix-argument (quote default))
+ '(neo-theme (quote arrow))
  '(powerline-color1 "#3d3d68")
  '(powerline-color2 "#292945"))
 (custom-set-faces

90ce72174d55f465d7eb1b15ecf6570b3e5fa26a
Akshay Mishra <lck.akshay@gmail.com>
2016-06-25 21:49:12 -0400

    Removed temporary su elevation

--- init.el
+++ init.el
@@ -384,3 +384,15 @@
 (defun configure/nix-global ()
   (interactive)
   (find-file "/etc/nixos/configuration.nix"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(python-shell-interpreter "python"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
